14673d3a8d4a0285666d2e53735e00ec9341b506
gwz <874664974@qq.com>
2019-07-12 15:57:23 +0800

    advice的dao层

--- book-manager/book-manager-dao/src/test/java/jdbcTest/ConnTest.java
+++ book-manager/book-manager-dao/src/test/java/jdbcTest/ConnTest.java
@@ -1,6 +1,9 @@
 package jdbcTest;
 
+import com.book.dao.IAdviceDao;
+import com.book.dao.impl.AdviceDaoImpl;
 import com.book.pojo.Admin;
+import com.book.pojo.Advice;
 import com.book.util.JDBCUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
@@ -14,15 +17,28 @@ public class ConnTest {
     @Test
     public void demoTest(){
         QueryRunner qr = new QueryRunner(JDBCUtils.getDataSource());
-        List<Admin> li = null;
+        List<Advice> li = null;
         try {
-            li = qr.query("select * from `admin`", new BeanListHandler<>(Admin.class));
+            li = qr.query("select * from advice", new BeanListHandler<>(Advice.class));
         } catch (SQLException e) {
             e.printStackTrace();
         }
         if(li.get(0)==null){
             return;
         }
-        System.out.println(li.get(0).getAdmin_Id());
+        System.out.println(li.get(0));
+    }
+    @Test
+    public void demo1Test(){
+        IAdviceDao adviceDao = new AdviceDaoImpl();
+        List<Advice> adviceList = null;
+        try {
+            adviceList = adviceDao.selectAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        for(Advice advice:adviceList){
+            System.out.println(advice);
+        }
     }
 }
