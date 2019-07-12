932d3c7b578a0f40c9d4ddae6d44ecd76c366b49
wolf <chenzilin97@gmail.com>
2019-07-12 16:06:27 +0800

    package com.book.dao;
    public interface IBookDao {
        //新增书籍
        public void insertBook(Book_Basic book);
    
        //更新书籍信息
        public void updateBook(Book_Basic book);
    
        //下架书籍
        public void deleteBookById(int book_Id);
    
        //根据ID 查询书籍
        public Book_Basic selectBookById(int book_Id);
    
        //根据分类 查询书籍
        public List<Book_Basic> selectBookById(int type_Id);
    
        //查询全部书籍
        public  List<Book_Basic> selectAllBook();
    }

---
+++ book-manager/book-manager-dao/src/main/java/com/book/dao/IBookDao.java
@@ -0,0 +1,23 @@
+package com.book.dao;
+
+import java.util.List;
+
+public interface IBookDao {
+    //新增书籍
+    public void insertBook(Book_Basic book);
+
+    //更新书籍信息
+    public void updateBook(Book_Basic book);
+
+    //下架书籍
+    public void deleteBookById(int book_Id);
+
+    //根据ID 查询书籍
+    public Book_Basic selectBookById(int book_Id);
+
+    //根据分类 查询书籍
+    public List<Book_Basic> selectBookById(int type_Id);
+
+    //查询全部书籍
+    public  List<Book_Basic> selectAllBook();
+}
