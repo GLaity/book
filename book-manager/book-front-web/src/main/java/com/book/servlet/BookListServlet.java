701b0e51777381c005e250053bfcabdc8fa86aa6
jhm <1393514590@qq.com>
2019-07-17 17:38:22 +0800

    Merge branch 'master' of https://github.com/GLaity/book into jhm
    # Conflicts:
    #	book-manager/book-front-web/src/main/webapp/ranking.jsp
    #	book-manager/book-manager-web/pom.xml
    #	book-manager/book-manager-web/src/main/java/com/book/servlet/ManagerServlet.java
    #	book-manager/book-manager-web/src/main/webapp/css/bootstrap.min.css

--- book-manager/book-front-web/src/main/java/com/book/servlet/BookListServlet.java
+++ book-manager/book-front-web/src/main/java/com/book/servlet/BookListServlet.java
@@ -1,19 +1,65 @@
 package com.book.servlet;
 
+import com.book.pojo.Book_Basic;
+import com.book.pojo.Book_Type;
+import com.book.service.IBookService;
+import com.book.service.impl.BookServiceImpl;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
+@WebServlet("/list")
 public class BookListServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String condition = req.getParameter("condition");
+        if(condition == null){
+            execute(req,resp);
+        } else {
+            switch (condition){
+                case "search":
+                    String keyName = req.getParameter("value");
+                    search(req,resp,keyName);
+                    break;
+                case "type":
+                    int typeId = Integer.valueOf(req.getParameter("value"));
+                    type(req, resp, typeId);
+                    break;
+            }
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
+    }
+
+    public void execute(HttpServletRequest req, HttpServletResponse resp) throws  ServletException,IOException {
+        IBookService bookService = new BookServiceImpl();
+        List<Book_Basic> bookBasicList = bookService.findBookBasicAll();
+        req.setAttribute("bookBasicList",bookBasicList);
+        req.getRequestDispatcher("bookList.jsp").forward(req,resp);
+    }
+
+    public void type(HttpServletRequest req, HttpServletResponse resp, int typeId) throws  ServletException,IOException {
+        IBookService bookService = new BookServiceImpl();
+        List<Book_Basic> bookBasicList = bookService.findBookByTypeId(typeId);
+        Book_Type bookType = bookService.fintBookTypeByTypeId(typeId);
+        req.setAttribute("bookType",bookType);
+        req.setAttribute("bookBasicList",bookBasicList);
+        req.getRequestDispatcher("bookList.jsp").forward(req,resp);
+    }
+
+    public void search(HttpServletRequest req, HttpServletResponse resp, String  keyName) throws  ServletException,IOException {
+        IBookService bookService = new BookServiceImpl();
+        List<Book_Basic> bookBasicList = bookService.findBookBySearchName(keyName);
+        req.setAttribute("keyName",keyName);
+        req.setAttribute("bookBasicList",bookBasicList);
+        req.getRequestDispatcher("bookList.jsp").forward(req,resp);
     }
 }
